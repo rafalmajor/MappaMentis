015fe603170064c90a9a4158522094e00e664376
rafal <rafal.major@gmail.com>
2025-11-23 23:42:04 +0100

    Viabe code splash screen

---
+++ src/MappaMentis.Wpf/App.xaml.cs
@@ -0,0 +1,31 @@
+using System.Windows;
+
+namespace MappaMentis.Wpf;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        // Show splash screen
+        SplashScreen splashScreen = new SplashScreen();
+        splashScreen.Show();
+
+        // Create and show main window
+        MainWindow mainWindow = new MainWindow();
+        
+        // Close splash screen and show main window after a delay
+        System.Threading.Tasks.Task.Delay(2000).ContinueWith(_ =>
+        {
+            Dispatcher.Invoke(() =>
+            {
+                mainWindow.Show();
+                splashScreen.Close();
+            });
+        });
+    }
+}
---
+++ src/MappaMentis.Wpf/SplashScreen.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+
+namespace MappaMentis.Wpf;
+
+/// <summary>
+/// Interaction logic for SplashScreen.xaml
+/// </summary>
+public partial class SplashScreen : Window
+{
+    public SplashScreen()
+    {
+        InitializeComponent();
+    }
+}
