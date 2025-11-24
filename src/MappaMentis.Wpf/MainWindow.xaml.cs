9d55ac55cd03706c708ae8153ce03761bf215cb4
rafal <rafal.major@gmail.com>
2025-11-24 22:10:47 +0100

    Use CommunityToolkit.MVVM

--- src/MappaMentis.Wpf/MainWindow.xaml.cs
+++ src/MappaMentis.Wpf/MainWindow.xaml.cs
@@ -8,7 +8,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MappaMentis.Wpf.Views;
 
 namespace MappaMentis.Wpf;
 
@@ -20,12 +19,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-    }
-
-    private void GetStartedButton_Click(object sender, RoutedEventArgs e)
-    {
-        DashboardView dashboardView = new DashboardView();
-        dashboardView.Show();
-        this.Close();
     }
 }
---
+++ src/MappaMentis.Wpf/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,24 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MappaMentis.Wpf.Views;
+
+namespace MappaMentis.Wpf.ViewModels;
+
+/// <summary>
+/// ViewModel for Welcome/MainWindow content
+/// </summary>
+public partial class WelcomeViewModel : ObservableObject
+{
+    public string WelcomeTitle => "Welcome to MappaMentis";
+    public string WelcomeSubtitle => "Your Mind Mapping Application";
+
+    [RelayCommand]
+    private void GetStarted()
+    {
+        DashboardView dashboardView = new DashboardView();
+        dashboardView.Show();
+
+        // Close the main window
+        System.Windows.Application.Current.MainWindow?.Close();
+    }
+}
